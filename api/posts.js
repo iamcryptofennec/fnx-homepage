fc2f9eccabf25ba7e5ba25e8f4191f8b65e1148c
noahniuwa <noahmaizels@protonmail.com>
2020-12-03 00:11:37 -0700

    ..

--- api/posts.js
+++ api/posts.js
@@ -1,29 +1,29 @@
-// import GhostContentAPI from "@tryghost/content-api"
+import GhostContentAPI from "@tryghost/content-api"
 
 
-// // Create API instance with site credentials
-// const api = new GhostContentAPI({
-//   url: 'https://ghost-finnexus.herokuapp.com',
-//   key: 'a4c8b784c4018f771dca51f99e',
-//   version: "v3"
-// })
+// Create API instance with site credentials
+const api = new GhostContentAPI({
+  url: 'https://ghost-finnexus.herokuapp.com',
+  key: 'a4c8b784c4018f771dca51f99e',
+  version: "v3"
+})
 
-// export async function getPosts() {
-//   return await api.posts
-//     .browse({
-//       limit: "all"
-//     })
-//     .catch(err => {
-//       console.error(err)
-//     })
-// }
+export async function getPosts() {
+  return await api.posts
+    .browse({
+      limit: "all"
+    })
+    .catch(err => {
+      console.error(err)
+    })
+}
 
-// export async function getSinglePost(postSlug) {
-//   return await api.posts
-//     .read({
-//       slug: postSlug
-//     })
-//     .catch(err => {
-//       console.error(err);
-//     });
-// }
+export async function getSinglePost(postSlug) {
+  return await api.posts
+    .read({
+      slug: postSlug
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,35 +1,35 @@
-// // Import getSinglePost function
-// import { getSinglePost } from '../../api/posts';
+// Import getSinglePost function
+import { getSinglePost } from '../../api/posts';
 
-// // PostPage page component
-// const PostPage = (props) => {
-//   // Render post title and content in the page from props
-//   return (
-//     <div>
-//       <h1>{props.post.title}</h1>
-//       <div dangerouslySetInnerHTML={{ __html: props.post.html }} />
-//     <style jsx>
-//       {`
-//         p, h1 {
-//             font-size: 62px;
-//           }
-//           h2 {
-//             font-size: 1.5rem;
-//           }
-//           h3 {
-//             font-size: 1.3rem;
-//           }
-//       `}
-//     </style>
-//     </div>
-//   )
-// }
+// PostPage page component
+const PostPage = (props) => {
+  // Render post title and content in the page from props
+  return (
+    <div>
+      <h1>{props.post === undefined ? "" : props.post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: props.post === undefined ? "" : ""}} />
+    <style jsx>
+      {`
+        p, h1 {
+            font-size: 62px;
+          }
+          h2 {
+            font-size: 1.5rem;
+          }
+          h3 {
+            font-size: 1.3rem;
+          }
+      `}
+    </style>
+    </div>
+  )
+}
 
-// // Pass the page slug over to the "getSinglePost" function
-// // In turn passing it to the posts.read() to query the Ghost Content API
-// PostPage.getInitialProps = async (params) => {
-//   const post = await getSinglePost(params.query.slug);
-//   return { post: post }
-// };
+// Pass the page slug over to the "getSinglePost" function
+// In turn passing it to the posts.read() to query the Ghost Content API
+PostPage.getInitialProps = async (params) => {
+  const post = await getSinglePost(params.query.slug);
+  return { post: post }
+};
 
-// export default PostPage
+export default PostPage
